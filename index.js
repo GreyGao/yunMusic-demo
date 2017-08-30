eded5c6d68c59aa22416be78757e845fa4d1567e
GreyGao <rh87116938@163.com>
2017-08-31 00:57:15 +0800

    大幅优化搜索栏功能，基本完成搜索功能

--- index.js
+++ index.js
@@ -42,46 +42,112 @@ query.find().then(function (results) {
 
 // 歌曲搜索
 let resultList = document.querySelector('#resultList');
-$('input#searchSong').on('input',function (e) {
+$('input#searchSong').bind('input',function (e) {
+    inputChange(e)
+}).bind('focus',function (e) {
+    inputChange(e)
+}).bind('keypress',function (e) {
+    searchSubmit(e)
+});
 
-    let $input = $(e.currentTarget);
-    let value = $input.val().trim();
-    let query = new AV.Query('Song');
-  // query.contains('name',value);
-    query.contains('des',value);
+$('#hotSearchList').on('click','li',function (e) {
+    hotSongLink(e)
+});
 
-    if(value.length === 0){
-        $('#searchTips').removeClass('hide');
-        $('#searchResults').addClass('hide');
-        $('#search-holder').text('搜索歌曲、歌手、专辑');
+let timer = null;
 
-    }else{
-        $('#searchTips').addClass('hide');
-        $('#search-holder').text('');
-        query.find().then(function (results) {
-            $('#resultList').empty();
-            if (results.length === 0) {
-                let li =     `
+function inputChange(e) {
+    $('.resultMatch').addClass('hide');
+    $('#searchResults').addClass('hide');
+    $('#search-holder').text('');
+    if(timer){window.clearTimeout(timer)}
+    timer = setTimeout(function () {
+        timer = null;
+        let $input = $(e.currentTarget);
+        let value = $input.val().trim();
+        let nameQuery = new AV.Query('Song');
+        nameQuery.contains('name', value);
+        let singerQuery = new AV.Query('Song');
+        singerQuery.contains('singer', value);
+        let albumQuery = new AV.Query('Song');
+        albumQuery.contains('album', value);
+        let desQuery = new AV.Query('Song');
+        desQuery.contains('des', value);
+        let query = AV.Query.or(nameQuery, singerQuery, albumQuery, desQuery);
+        if (value.length === 0) {
+            $('#search-holder').text('搜索歌曲、歌手、专辑');
+            $('#searchTips').removeClass('hide');
+        } else {
+            $('#searchTips').addClass('hide');
+            query.find().then(function (results) {
+                $('#resultList').empty();
+                if (results.length === 0) {
+                    let li = `
                 <li class="resultSong"><i class="svg svg-search"></i><span>没有结果</span></li>
                 `;
-                resultList.insertAdjacentHTML('beforeend', li)
-            } else {
-                for (let i = 0; i < results.length; i++) {
-                    let song = results[i].attributes;
-                    let li =
-                        `
+                    resultList.insertAdjacentHTML('beforeend', li)
+                } else {
+                    for (let i = 0; i < results.length; i++) {
+                        let song = results[i].attributes;
+                        let li =
+                            `
                 <li class="resultSong"><i class="svg svg-search"></i><span>${song.name} - ${song.singer}</span></li>
                 `;
-                    resultList.insertAdjacentHTML('beforeend', li)
+                        resultList.insertAdjacentHTML('beforeend', li)
+                    }
                 }
-            }
-        });
-        $('#searchResults').removeClass('hide');
-        $('#searchLink').text(`搜索"${value}"`)
-    }
+            });
+            $('#searchResults').removeClass('hide');
+            $('#searchLink').text(`搜索"${value}"`)
+        }
+    },400)
+}
 
+function hotSongLink(e) {
+    let $li = $(e.currentTarget);
+    let value = $li.text().trim();
+    matchSongList(value)
+}
 
-});
+function searchSubmit(e){
+    let $input = $(e.currentTarget);
+    let value = $input.val().trim();
+    matchSongList(value);
+    $('#searchResults').addClass('hide')
+}
+
+function matchSongList(value) {
+    $('.resultMatch').removeClass('hide');
+    $('#searchTips').addClass('hide');
+    let matchSongList = document.querySelector('#matchSongList');
+    $('input#searchSong').val(value);
+    $('#search-holder').text('');
+    let nameQuery = new AV.Query('Song');
+    nameQuery.contains('name', value);
+    let singerQuery = new AV.Query('Song');
+    singerQuery.contains('singer', value);
+    let albumQuery = new AV.Query('Song');
+    albumQuery.contains('album', value);
+    let desQuery = new AV.Query('Song');
+    desQuery.contains('des', value);
+    let query = AV.Query.or(nameQuery, singerQuery, albumQuery, desQuery);
+
+    query.find().then(function (results) {
+        $('#matchSongList').empty();
+        for (let i = 0; i < results.length; i++) {
+            let song = results[i].attributes;
+            let div =
+                `<div class="songInfo">
+             <p class="songTitle">${song.name}<span class="songDesc">${song.des}</span></p>
+                            <p class="singer"><i class="icon icon-sq"></i>${song.singer} - ${song.album}</p>
+                            <div class="playButton"><i class="icon icon-play"></i></div>   
+                
+            `;
+            matchSongList.insertAdjacentHTML('beforeend', div)
+        }
+    })
+}
+
 
 
 // 添加歌曲数据库API
